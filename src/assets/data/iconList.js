65c66dccabbd998dc9ce369b7bf8391fb9efaac2
Murtaza01 <merdada2000@hotmail.com>
2025-01-17 02:18:07 +0300

    added skills, skillItem componenet, style it and everything, added main color (purple)

---
+++ src/assets/data/iconList.js
@@ -0,0 +1,17 @@
+const iconList = [
+  { name: "latex", hoverColor: "#171717", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "react", hoverColor: "#06B6D4", shapeColor: ["#fff", "#0284c7"] },
+  { name: "astro", hoverColor: "#0F172A", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "css", hoverColor: "#3B82F6", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "html", hoverColor: "#C2410C", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "javascript", hoverColor: "#FACC15", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "mongodb", hoverColor: "#22C55E", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "mysql", hoverColor: "#1D4ED8", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "node", hoverColor: "#22C55E", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "tailwind", hoverColor: "#0E7490", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "typescript", hoverColor: "#3B82F6", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "arch", hoverColor: "#0891B2", shapeColor: ["#ffffff", "#9333ea"] },
+];
+
+
+export default iconList
