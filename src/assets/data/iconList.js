3a9842545c4377d68f8de77c1a8f34c7f9f23e5a
Murtaza01 <merdada2000@hotmail.com>
2025-01-17 22:29:51 +0300

    finished the skills compoenents, added a sepreate file for the iconList, made every skill has its own uniqe blob shape colo

--- src/assets/data/iconList.js
+++ src/assets/data/iconList.js
@@ -1,16 +1,16 @@
 const iconList = [
-  { name: "latex", hoverColor: "#171717", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "react", hoverColor: "#06B6D4", shapeColor: ["#fff", "#0284c7"] },
-  { name: "astro", hoverColor: "#0F172A", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "css", hoverColor: "#3B82F6", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "html", hoverColor: "#C2410C", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "javascript", hoverColor: "#FACC15", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "mongodb", hoverColor: "#22C55E", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "mysql", hoverColor: "#1D4ED8", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "node", hoverColor: "#22C55E", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "tailwind", hoverColor: "#0E7490", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "typescript", hoverColor: "#3B82F6", shapeColor: ["#ffffff", "#9333ea"] },
-  { name: "arch", hoverColor: "#0891B2", shapeColor: ["#ffffff", "#9333ea"] },
+  { name: "latex", hoverColor: "#171717", shapeColor: ["#A855F7", "#743AAB"] },
+  { name: "arch", hoverColor: "#0891B2", shapeColor: ["#B37407", "#B33B07"] },
+  { name: "astro", hoverColor: "#334155", shapeColor: ["#544432", "#939CA9"] },
+  { name: "react", hoverColor: "#38BDF8", shapeColor: ["#F77E39", "#F7B939"] },
+  { name: "html", hoverColor: "#C2410C", shapeColor: ["#0CA5C2", "#0CC265"] },
+  { name: "css", hoverColor: "#3B82F6", shapeColor: ["#F5B83B", "#F59F3B"] },
+  { name: "javascript", hoverColor: "#FACC15", shapeColor: ["#5414FA ", "#1468FA"] },
+  { name: "mongodb", hoverColor: "#16A34A", shapeColor: ["#A33415", "#A31555"] },
+  { name: "mysql", hoverColor: "#1D4ED8", shapeColor: ["#D98C1C", "#D9BA1C"] },
+  { name: "node", hoverColor: "#22C55E", shapeColor: ["#C4216B", "#C4216B"] },
+  { name: "tailwind", hoverColor: "#0D9488", shapeColor: ["#94510D", "#94220D"] },
+  { name: "typescript", hoverColor: "#3B82F6", shapeColor: ["#F5CE3B", "#F59F3B"] },
 ];
 
 
