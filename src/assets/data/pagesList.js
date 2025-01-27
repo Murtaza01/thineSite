fb7c60eb3e12e87ee3b6f28ca1f0fb5a0088568b
Murtaza01 <merdada2000@hotmail.com>
2025-01-27 07:44:42 +0300

    changed the stuff component into a better version with hexagon shapes and images, around 4 hours of work removed into this new component, way easier also

---
+++ src/assets/data/pagesList.js
@@ -0,0 +1,14 @@
+import autechre from "../images/autechre.avif"
+import faustus from "../images/faustus.avif"
+import all_is_lost from "../images/all_is_lost.avif"
+import socrates from "../images/socrates.avif"
+import odyssey from "../images/odyssey.avif"
+const pagesItems = [
+  {title:"music",bg:`url(${autechre.src})`},
+  {title:"books",bg:`url(${odyssey.src})`},
+  {title:"movies",bg:`url(${all_is_lost.src})`},
+  {title:"college",bg:`url(${faustus.src})`},
+  {title:"gallery",bg:`url(${socrates.src})`},
+]
+
+export default pagesItems
